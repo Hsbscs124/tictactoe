c1f59c04655d626a30577dd57c7628390b2cbfc3
Hsbscs124 <moserrmarkus187@gmail.com>
2026-01-09 11:07:44 +0100

    a

---
+++ tictac/src/main/java/com/example/tictac/tictactoe.java
@@ -0,0 +1,18 @@
+package com.example.tictac;
+
+public class tictactoe {
+
+        public static void main(String[] args) {
+
+            char[][] board = {
+                    {' ', ' ', ' '},
+                    {' ', ' ', ' '},
+                    {' ', ' ', ' '}
+            };
+
+
+
+
+
+        }
+}
