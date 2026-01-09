44c7f54352beb795d5e74f1334af4a551743c370
Hsbscs124 <moserrmarkus187@gmail.com>
2026-01-09 11:12:51 +0100

    a

--- tictac/src/main/java/com/example/tictac/tictactoe.java
+++ tictac/src/main/java/com/example/tictac/tictactoe.java
@@ -2,17 +2,37 @@ package com.example.tictac;
 
 public class tictactoe {
 
-        public static void main(String[] args) {
-
-            char[][] board = {
-                    {' ', ' ', ' '},
-                    {' ', ' ', ' '},
-                    {' ', ' ', ' '}
-            };
-
+    public static void main(String[] args) {
+        char[][] board = {
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
 
 
 
+        printBoard(board);
+    }
 
+    static void printBoard(char[][] board) {
+        int field = 1;
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+
+                char out = (board[row][col] == ' ')
+                        ? (char) ('0' + field)
+                        : board[row][col];
+
+                System.out.print(" " + out + " ");
+                if (col < 2) System.out.print("|");
+
+                field++;
+            }
+
+            System.out.println();
+            if (row < 2) System.out.println("---+---+---");
         }
+    }
 }
+
